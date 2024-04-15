5ce461c420846e1f089239fba47e327f72d4d43a
Miguel Gonçalves <miguel.goncalves@corebiz.ag>
2024-04-15 18:33:45 -0300

    feat: me, phone and createSupportTicket APIs

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,16 +3,34 @@ Complete the following API functions to fetch user's data and its unmasked phone
 Each request should be authenticated with a Bearer token of 'WellTheoryCode2023'.
 Use the default fetch API.
 */
+const CONFIG = {
+  headers: {
+    Authorization: 'Bearer WellTheoryCode2023'
+  }
+}
 
-export const me = () => {
-  // GET https://us-central1-internals-358114.cloudfunctions.net/react-challenge/me
+
+export const me = async () => {
+  const response = await fetch('https://us-central1-internals-358114.cloudfunctions.net/react-challenge/me', CONFIG)
+  return response.json()
 };
 
-export const phone = () => {
-  // GET https://us-central1-internals-358114.cloudfunctions.net/react-challenge/phone
+export const phone = async () => {
+  const response = await fetch('https://us-central1-internals-358114.cloudfunctions.net/react-challenge/phone', CONFIG)
+  return response.json()
 };
 
-export const createSupportTicket = () => {
-  // POST https://us-central1-internals-358114.cloudfunctions.net/react-challenge/support-tickets
+export const createSupportTicket = async (
+  { title, message }: {title: string, message: string}
+) => {
+  // POST 
   // body: { title: string; message: string }
+  const response = await fetch('https://us-central1-internals-358114.cloudfunctions.net/react-challenge/support-tickets',{
+    ...CONFIG,
+    method: 'POST',
+    body: JSON.stringify({
+      title, message
+    })
+  } )
+  return response.json()
 };
